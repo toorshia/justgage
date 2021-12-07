e72abbba419ed13fe36dfcb3bdec821f948bbf6c
Michael Rochelle <michael@j2inn.com>
2021-12-07 10:24:36 -0800

    Created logic to generate uuid for each component to prevent a style conflict.

--- justgage.js
+++ justgage.js
@@ -63,7 +63,7 @@
       
       // classId : string
       // this is the class id utilize when generating styles
-      classId: config.classId,
+      classId: uuid(),
 
       // value : float
       // value gauge is showing
@@ -1575,6 +1575,21 @@
 
   function isNumber(n) {
     return n !== null && n !== undefined && !isNaN(n);
+  }
+
+  /**
+   * Generate UUID
+   * @returns UUID
+   */
+  function uuid() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        const r = (Math.random() * 16) | 0;
+        const v = c === "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
   }
 
   return JustGage;
